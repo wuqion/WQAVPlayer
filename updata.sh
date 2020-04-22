b05cd561d3675cd2fa47436a36ccb9eea9f69e8a
吴琼 <wuqiongaia@163.com>
2020-04-23 02:45:20 +0800

    提交

--- updata.sh
+++ updata.sh
@@ -1,1 +1,1 @@
-pod cache clean --all || git tag -d '0.2.0' || git push origin :0.2.0 && git add . && git commit -m "提交" && git push && git tag '0.2.0' && git push --tags && pod repo push WQSpec WQAVPlayer.podspec
+pod cache clean --all && git tag -d '0.2.0' && git push origin :0.2.0 && git add . && git commit -m "提交" && git push && git tag '0.2.0' && git push --tags && pod repo push WQSpec WQAVPlayer.podspec
